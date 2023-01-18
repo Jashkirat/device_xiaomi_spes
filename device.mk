00f5be29dc6f649b00e7a286568a2340bfd62563
David <davidmolinamorales1999@gmail.com>
2023-01-18 20:21:58 +0000

    spes: Build Missing libs needed by GPS HAL
    03-24 13:29:51.704   450   487 E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library libloc_net_iface.so not found
    03-24 13:29:51.509   450   450 E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library liblocdiagiface.so not found
    
    Signed-off-by: takumi021 <omrameshpatel@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -232,7 +232,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
-    android.hardware.gnss@2.1.vendor
+    android.hardware.gnss@2.1.vendor \
+    libloc_net_iface \
+    liblocdiagiface
 
 PRODUCT_PACKAGES += \
     android.hardware.gnss.measurement_corrections@1.0.vendor \
