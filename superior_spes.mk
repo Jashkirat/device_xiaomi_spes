35e9973c9e8b2cd336f82fbabfba1b73c105ad99
jashkirat <jashkiratsingh@gmail.com>
2023-10-19 15:39:42 +0000

    Superiorfying Spes

--- superior_spes.mk
+++ superior_spes.mk
@@ -11,25 +11,26 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/blaze/config/common_full_phone.mk)
+# Inherit some common SuperiorOS Extended stuff.
+$(call inherit-product, vendor/superior/config/common.mk)
 
 # Product Specifics
-PRODUCT_NAME := blaze_spes
+PRODUCT_NAME := superior_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Boot animaton
+# Superior Official Stuff
 TARGET_BOOT_ANIMATION_RES := 1080
-
-# Blaze Official Stuff
-BLAZE_BUILD_TYPE := OFFICIAL
-BLAZE_MAINTAINER := sayann70
 TARGET_FACE_UNLOCK_SUPPORTED := true
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+TARGET_INCLUDE_PIXEL_CHARGER := true
+BUILD_WITH_GAPPS := true
+
+# Maintainer
+PRODUCT_SYSTEM_PROPERTIES += \
+     ro.spos.maintainer=Jashkirat Virdi
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
