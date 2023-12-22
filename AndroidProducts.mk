4ea403a52b01321923bb88fba17c0854ff924564
Jashkirat <jashkiratsingh@gmail.com>
2023-12-22 07:16:33 +0000

    spes: Say hello to SuperiorOS Extended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/blaze_spes.mk
+    $(LOCAL_DIR)/superiro_spes.mk
 
 COMMON_LUNCH_CHOICES := \
-    blaze_spes-user \
-    blaze_spes-userdebug \
-    blaze_spes-eng
+    superior_spes-user \
+    superior_spes-userdebug \
+    superior_spes-eng
