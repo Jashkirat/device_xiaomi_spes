d0b89f5c1895129c51a8c91a8938ac4a3c6a18dd
chrisl7 <wandersonrodriguesf1@gmail.com>
2024-01-14 23:10:25 +0000

    spes: Move hals.conf to device tree
    Change-Id: Ic783df474a85784ae971d2f7e22adfed63c80a5c
    Signed-off-by: chrisl7 <wandersonrodriguesf1@gmail.com>
    Signed-off-by: tanvirr007 <43490582+tanvirr007@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -467,6 +467,10 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     libsensorndkbridge
 
+# Sensors
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
