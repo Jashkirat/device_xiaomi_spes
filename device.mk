1614ba69f10aabfc7b367ed8ef8c863b4ca4479b
AmritoRock <amritokarmokar5i@gmail.com>
2024-01-21 10:13:20 +0600

    spes: Build libloc_net_iface vendor variant
    E LocSvc_misc_utils: logDlError:129] dlopen error: dlopen failed: library "libloc_net_iface.so" not found
    
    Signed-off-by: Amritorock <amritokarmokar5i@gmail.com>
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -237,6 +237,7 @@ PRODUCT_PACKAGES += \
     android.hardware.gnss@1.1.vendor \
     android.hardware.gnss@2.1.vendor \
     libloc_net_iface \
+    libloc_net_iface.vendor \
     liblocdiagiface
 
 PRODUCT_PACKAGES += \
