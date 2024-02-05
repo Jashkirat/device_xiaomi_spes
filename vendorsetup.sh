2d2b5ecdc68ef6cf310ac21d0985117d6d5818b7
Jashkirat <jashkiratsingh@gmail.com>
2024-02-05 00:25:46 -0500

    spes: Bringup @SuperiorExtended
    Bringup @SuperiorExtended
    
    Co-Authored-By: Sayan Sarkar <sayansarkarda2@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,4 +11,4 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Kernel & Vendor Sources
 git clone https://github.com/ProjectBlaze-Devices/vendor_xiaomi_spes vendor/xiaomi/spes
-git clone https://github.com/mi-sdm680/android_kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
