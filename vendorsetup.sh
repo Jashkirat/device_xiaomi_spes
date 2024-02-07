f1c206a720d1cd079845e1850f8b3ad8277bd74e
muralivijay <muralidharan9845@gmail.com>
2024-02-08 01:13:39 +0530

    spes: Fix imsrcsservice neverallow
    * Lineage OS qcom sepolicy recently added https://github.com/LineageOS/android_device_qcom_sepolicy_vndr/commit/6f4cd84d0892b9774bcc10e6c4f111131a3c3821
    * Actually we need allow vendor_hal_rcsservice instead of other ims HIDL.
    * Even tried compile with BOARD_USES_LEGACY_IMS_SEPOLICY := true . its still not compiling from legacy-ims policy.
    * So revert this changes from device/qcom/sepolicy_vndr/lagacy-um . for make sure compile ims policy for vendor_hal_rcsservice and not others.
    
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -11,3 +11,8 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
+
+# Sepolicy fix for imsrcsd
+echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
+rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
+cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
