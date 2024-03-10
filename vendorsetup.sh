cbf4a281a8cee4a378e22bc5962a488cbac1eaac
sayann70 <sayansarkarda2@gmail.com>
2024-03-10 19:09:33 +0000

    spes: Configure vendorsetup.sh to clone kernel & vendor sources
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -12,6 +12,10 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
 
+# Kernel & Vendor Sources
+git clone https://github.com/sayann70/vendor_xiaomi_spes -b A14 vendor/xiaomi/spes
+git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+
 # Sepolicy fix for imsrcsd
 echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
 rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
