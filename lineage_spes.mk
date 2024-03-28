418f2802b029090c21adeec9e8efd5d4f003f1fe
sayann70 <sayansarkarda2@gmail.com>
2024-03-28 08:54:47 +0000

    Revert "spes: Don,t build lineage livedisplay HAL."
    This reverts commit 611984000b065ceb611598d0fa8c9e5d6dda42c6.

--- lineage_spes.mk
+++ lineage_spes.mk
@@ -29,5 +29,9 @@ TARGET_BOOT_ANIMATION_RES := 1080
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
 # Fingerprint
 BUILD_FINGERPRINT := "Redmi/spes/spes:13/TKQ1.221114.001/V14.0.5.0.TGCINXM:user/release-keys"
