feff89a6b46c22f8e053d541f5fcce1b4dfabb1b
mikeNG <mkbestas@lineageos.org>
2024-06-21 12:39:43 -0300

    spes: Build missing libraries for 14 QPR3
    Change-Id: I6a84526ca1e9f4e5140c8068646433cab3fe85b2
    
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -144,8 +144,10 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    libutilscallstack.vendor \
     android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service_64
+    android.hardware.camera.provider@2.4-service_64 \
+    libpng.vendor
 
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor \
@@ -401,10 +403,13 @@ PRODUCT_COPY_FILES += \
 
 # QMI
 PRODUCT_PACKAGES += \
+    libcurl.vendor \
     libjson \
+    libjsoncpp.vendor \
     libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
     libvndfwk_detect_jni.qti \
+    libsqlite.vendor \
     libvndfwk_detect_jni.qti.vendor
 
 # Quick Tap
