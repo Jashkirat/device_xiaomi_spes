f5299ec729309a8fdb2f397442120111bab4520d
sayann70 <sayansarkarda2@gmail.com>
2024-11-12 00:56:41 +0530

    spes: Update vendorsetup.sh
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -13,7 +13,7 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 rm -rf hardware/lineage/compat/Android.bp
 
 # Kernel & Vendor Sources
-git clone https://github.com/ofcsayan/vendor_xiaomi_spes -b 15 vendor/xiaomi/spes
+git clone https://github.com/sayann70/vendor_xiaomi_spes -b 15 vendor/xiaomi/spes
 git clone https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
 
 # MiuiCamera
@@ -25,6 +25,9 @@ rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
 cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservice.te device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/hal_rcsservice.te
 
 # Custom KeyGen
-curl -O https://raw.githubusercontent.com/ofcsayan/Key-Gen-signed-script/main/generate_all_keys.sh
-chmod +x generate_all_keys.sh
-./generate_all_keys.sh
+#curl -O https://raw.githubusercontent.com/ofcsayan/Key-Gen-signed-script/main/generate_all_keys.sh
+#chmod +x generate_all_keys.sh
+#./generate_all_keys.sh
+
+# Rename conflicting qti_kernel_headers in source
+sed -i 's/"qti_kernel_headers"/"qti_kernel_headers_old"/g' vendor/lineage/build/soong/Android.bp
