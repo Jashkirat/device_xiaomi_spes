79553eef02cd04a68d325a922a205491bdf3791a
angelomds42 <angelomds42@gmail.com>
2024-11-13 08:55:57 -0300

    spes: Drop useless displayfeature
    Signed-off-by: angelomds42 <angelomds42@gmail.com>

--- device.mk
+++ device.mk
@@ -219,7 +219,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
     vendor.qti.hardware.display.mapperextensions@1.1.vendor \
-    vendor.xiaomi.hardware.displayfeature@1.0.vendor
+    vendor.qti.hardware.memtrack-service
 
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
