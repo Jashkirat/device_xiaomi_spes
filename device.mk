2c53c46a1f1676e5acb9ff9caeddf10c299ee4a8
nattolecats <nattolecats@users.noreply.github.com>
2025-02-01 16:14:14 +0530

    spes: Link libwvhidl against libcrypto from vndk 33
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- device.mk
+++ device.mk
@@ -225,7 +225,7 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4.vendor \
     android.hardware.drm-service.clearkey \
-    libcrypto_shim.vendor
+    libcrypto-v33
 
 # Fastbootd
 PRODUCT_PACKAGES += \
--- extract-files.sh
+++ extract-files.sh
@@ -64,9 +64,9 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             llvm-strip --strip-debug  "${2}"
             ;;
-        vendor/lib64/mediadrm/libwvdrmengine.so|vendor/lib64/libwvhidl.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libcrypto_shim.so" "${2}" || "${PATCHELF}" --add-needed  "libcrypto_shim.so" "${2}"
+        vendor/lib64/libwvhidl.so)
+            "${PATCHELF}" --replace-needed "libcrypto.so" "libcrypto-v33.so" "libcrypto-v34.so" "${2}"
+            "${PATCHELF}" --set-soname "libcrypto-v34.so" "${2}"
             ;;
     esac
 }
