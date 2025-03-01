01d9a491de542cd241d4aa1a161e187996a03132
me-cafebabe <me.cafebabe@gmail.com>
2025-03-02 05:05:49 +0600

    spes: Build QTI Thermal AIDL HAL
    Change-Id: I21406af9d90fe6be1402f30e1b19958e4f46eec9
    Signed-off-by: tanvirr007 <tanvir.mymnsngh@gmail.com>

--- device.mk
+++ device.mk
@@ -91,6 +91,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.verified_boot.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.verified_boot.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2021-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal-service.qti
+
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService-Soong \
