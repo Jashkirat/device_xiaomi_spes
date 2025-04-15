c4b3c1e7ecb653a0fc37c14df3d6e3289895c0c6
Jabiyeff <cebiyevanar@gmail.com>
2025-04-15 19:49:41 +0400

    Revert "spes: Uprev android.hardware.audio.effect"
    This reverts commit 89671fbbe392fd6d6d33e09c39c3533b4094d121.
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -96,7 +96,7 @@ PRODUCT_PACKAGES += \
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio.effect@7.0-impl \
+    android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio-impl
 
