334a16e12f7eb2cb5318059dc3ced2e3288e2249
Jabiyeff <cebiyevanar@gmail.com>
2025-05-05 12:49:43 +0400

    spes: Build AIDL Health recovery HAL
    * Also, update init files for new AIDL Health HAL
    
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- device.mk
+++ device.mk
@@ -262,6 +262,7 @@ PRODUCT_PACKAGES += \
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health-service.qti \
+    android.hardware.health-service.qti_recovery \
     android.hardware.health@2.1.vendor
 
 # HIDL
