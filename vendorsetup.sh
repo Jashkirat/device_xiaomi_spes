fa391712295bdd68e2010c321ae2a8643d0c0e13
Jashkirat <jashkiratsingh@gmail.com>
2025-06-14 13:08:25 -0400

    spes:Update Vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+# ROM source patches
 
 color="\033[0;32m"
 end="\033[0m"
@@ -12,6 +12,17 @@ rm -rf hardware/google/pixel/kernel_headers/Android.bp
 # Remove hardware/lineage/compat to avoid conflicts
 rm -rf hardware/lineage/compat/Android.bp
 
+# Kernel & Vendor Sources
+git clone --depth=1 https://github.com/sayann70/vendor_xiaomi_spes -b 15 vendor/xiaomi/spes
+git clone --depth=1 https://github.com/muralivijay/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
+
+# Hardware/Xiaomi
+rm -fr hardware/xiaomi
+git clone https://github.com/spes-development/hardware_xiaomi hardware/xiaomi
+
+# Debug Tools
+git clone https://github.com/spes-development/hardware_samsung-extra_interfaces -b lineage-22 hardware/samsung-ext/interfaces
+
 # Sepolicy fix for imsrcsd
 echo -e "${color}Switch back to legacy imsrcsd sepolicy${end}"
 rm -rf device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/ims/imsservice.te
@@ -19,27 +30,3 @@ cp device/qcom/sepolicy_vndr/legacy-um/qva/vendor/bengal/legacy-ims/hal_rcsservi
 
 # Rename conflicting qti_kernel_headers in source
 sed -i 's/"qti_kernel_headers"/"qti_kernel_headers_old"/g' vendor/lineage/build/soong/Android.bp
-
-# Clone specific repositories for (spes|spesn)
-echo -e "${color}Cloning vendor and kernel trees for spes|spesn${end}"
-
-# Clone vendor tree
-if [ ! -d "vendor/xiaomi/spes" ]; then
-    git clone https://github.com/Jabiyeff/android_vendor_xiaomi_spes vendor/xiaomi/spes
-else
-    echo -e "${color}vendor/xiaomi/spes already exists, skipping clone${end}"
-fi
-
-# Clone kernel tree
-if [ ! -d "kernel/xiaomi/sm6225" ]; then
-    git clone https://github.com/Jabiyeff/kernel_xiaomi_sm6225 kernel/xiaomi/sm6225
-else
-    echo -e "${color}kernel/xiaomi/sm6225 already exists, skipping clone${end}"
-fi
-
-# Replace hardware/xiaomi
-echo -e "${color}Resetting hardware/xiaomi${end}"
-rm -rf hardware/xiaomi
-git clone https://github.com/LineageOS/android_hardware_xiaomi hardware/xiaomi
-
-echo -e "${color}All done.${end}"
