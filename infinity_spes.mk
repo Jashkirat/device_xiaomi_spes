797d2551901d6f484b2cd7d0e7c5754eb170b03d
Jashkirat <jashkiratsingh@gmail.com>
2025-08-10 02:07:06 -0400

    spes: BringUP @ProjectInfinity-X

--- infinity_spes.mk
+++ infinity_spes.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from spes/spesn device
 $(call inherit-product, device/xiaomi/spes/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Project Infinity-X stuff.
+$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := lineage_spes
+PRODUCT_NAME := infinity_spes
 PRODUCT_DEVICE := spes
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 11
@@ -23,8 +23,14 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
+
+# Project InfinityX Flags
+WITH_GAPPS := true
+INFINITY_MAINTAINER := JASSIV07
+INFINITY_BUILD_TYPE := OFFICIAL
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
