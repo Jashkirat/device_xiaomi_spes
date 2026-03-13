bc810bbad97baf8a5daf76effeaa1b427edb322a
sayann70 <sayansarkarda2@gmail.com>
2026-03-13 14:13:42 +0530

    spes: Set `TARGET_DISABLE_EPPE` as true
    - this might be a dirty hack to get rid of offending entries,but hey if it works it works.
    
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- infinity_spes.mk
+++ infinity_spes.mk
@@ -25,11 +25,13 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 # Boot animaton
 TARGET_BOOT_ANIMATION_RES := 1080
-
 # Project InfinityX Flags
 WITH_GAPPS := true
 INFINITY_MAINTAINER := JASSIV07
 INFINITY_BUILD_TYPE := OFFICIAL
+
+# Disable EPPE
+TARGET_DISABLE_EPPE := true
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
