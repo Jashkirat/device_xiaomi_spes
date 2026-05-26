cc556c134357a3a4ff8d8fb7f27a32787d096c73
muralivijay <muralidharan9845@gmail.com>
2026-05-26 16:52:27 +0000

    spes:libinit: Update Dalvik vm props for android 16
    Signed-off-by: muralivijay <muralidharan9845@gmail.com>

--- libinit/include/libinit_dalvik_heap.h
+++ libinit/include/libinit_dalvik_heap.h
@@ -16,6 +16,18 @@ typedef struct dalvik_heap_info {
     std::string heapminfree;
     std::string heapmaxfree;
     std::string heaptargetutilization;
+    std::string foreground-heap-growth-multiplier;
+    std::string usejit;
+    std::string jitmaxsize;
+    std::string jitinitialsize;
+    std::string jitthreshold;
+    std::string madvise.vdexfile.size;
+    std::string madvise.odexfile.size;
+    std::string usap_pool_enabled;
+    std::string usap_pool_size_min;
+    std::string usap_pool_size_max;
+    std::string usap_refill_threshold;
+    std::string usap_pool_refill_delay_ms;
 } dalvik_heap_info_t;
 
 void set_dalvik_heap(void);
--- libinit/libinit_dalvik_heap.cpp
+++ libinit/libinit_dalvik_heap.cpp
@@ -15,6 +15,18 @@
 #define HEAPMINFREE_PROP "dalvik.vm.heapminfree"
 #define HEAPMAXFREE_PROP "dalvik.vm.heapmaxfree"
 #define HEAPTARGETUTILIZATION_PROP "dalvik.vm.heaptargetutilization"
+#define FOREGROUND_HEAP_GROWTH_MULTIPLIER_PROP "dalvik.vm.foreground-heap-growth-multiplier"
+#define USEJIT_PROP "dalvik.vm.usejit"
+#define JITMAXSIZE_PROP "dalvik.vm.jitmaxsize"
+#define JITINITIALSIZE_PROP "dalvik.vm.jitinitialsize"
+#define JITTHRESHOLD_PROP "dalvik.vm.jitthreshold"
+#define MADVISE_VDEXFILE_SIZE_PROP "dalvik.vm.madvise.vdexfile.size"
+#define MADVISE_ODEXFILE_SIZE_PROP "dalvik.vm.madvise.odexfile.size"
+#define USAP_POOL_ENABLED_PROP "dalvik.vm.usap_pool_enabled"
+#define USAP_POOL_SIZE_MIN_PROP "dalvik.vm.usap_pool_size_min"
+#define USAP_POOL_SIZE_MAX_PROP "dalvik.vm.usap_pool_size_max"
+#define USAP_REFILL_THRESHOLD_PROP "dalvik.vm.usap_refill_threshold"
+#define USAP_REFILL_DELAY_MS_PROP "dalvik.vm.usap_pool_refill_delay_ms"
 
 #define GB(b) (b * 1024ull * 1024 * 1024)
 
@@ -53,4 +65,16 @@ void set_dalvik_heap() {
     property_override(HEAPTARGETUTILIZATION_PROP, dhi->heaptargetutilization);
     property_override(HEAPMINFREE_PROP, dhi->heapminfree);
     property_override(HEAPMAXFREE_PROP, dhi->heapmaxfree);
+    property_override(FOREGROUND_HEAP_GROWTH_MULTIPLIER_PROP, dhi->foreground-heap-growth-multiplier);
+    property_override(USEJIT_PROP, dhi->usejit);
+    property_override(JITMAXSIZE_PROP, dhi->jitmaxsize);
+    property_override(JITINITIALSIZE_PROP, dhi->jitinitialsize);
+    property_override(JITTHRESHOLD_PROP, dhi->jitthreshold);
+    property_override(MADVISE_VDEXFILE_SIZE_PROP, dhi->madvise.vdexfile.size);
+    property_override(MADVISE_ODEXFILE_SIZE_PROP, dhi->madvise.odexfile.size);
+    property_override(USAP_POOL_ENABLED_PROP, dhi->usap_pool_enabled);
+    property_override(USAP_POOL_SIZE_MIN_PROP, dhi->usap_pool_size_min);
+    property_override(USAP_POOL_SIZE_MAX_PROP, dhi->usap_pool_size_max);
+    property_override(USAP_REFILL_THRESHOLD_PROP, dhi->usap_refill_threshold);
+    property_override(USAP_REFILL_DELAY_MS_PROP, dhi->usap_pool_refill_delay_ms);
 }
