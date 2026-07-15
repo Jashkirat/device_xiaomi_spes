84eb86fb9fa19597670e48fe2acb5755928903ee
sayann70 <sayansarkarda2@gmail.com>
2026-07-15 07:45:21 +0530

    spes: Don't rm ha*/lin*/com*/Android.bp
    - this should have been done a long time ago ughhh
    03-11 10:32:17.694  1169  1169 F linker  : CANNOT LINK EXECUTABLE /vendor/bin/sensors.qti: library libprotobuf-cpp-lite-3.9.1.so not found: needed by main executable
    
    Signed-off-by: sayann70 <sayansarkarda2@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,9 +9,6 @@ sleep 1
 # Remove pixel headers to avoid conflicts
 rm -rf hardware/google/pixel/kernel_headers/Android.bp
 
-# Remove hardware/lineage/compat to avoid conflicts
-rm -rf hardware/lineage/compat/Android.bp
-
 # Kernel & Vendor Sources
 git clone --depth=1 https://github.com/sayann70/vendor_xiaomi_spes -b 16-QPR2 vendor/xiaomi/spes
 git clone --depth=1 https://github.com/muralivijay/kernel_xiaomi_spes -b main-a16-sm8250-base kernel/xiaomi/sm6225
